bf173832032d6f6deaabc7a53e4eabb2197586b4
Peter Yefi <peteryefi@gmail.com>
2022-06-03 23:57:12 -0400

    New Issue

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
-
-// most @actions toolkit packages have async methods
+const core = require("@actions/core");
+const github = require("@actions/github");
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput("issue", JSON.stringify(response.data));
   } catch (error) {
     core.setFailed(error.message);
   }
